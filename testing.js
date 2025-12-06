56f637f69c60df65ef3be6e16fab8350fc0de9cd
Suki <sparamso@gmail.com>
2025-12-06 10:14:51 -0500

    This is my first commit

---
+++ testing.js
@@ -0,0 +1,8 @@
+console.log("This is a test file for testing purposes.");
+
+let x = 10;
+let y = 20; 
+let sum = x + y;
+console.log("The sum of x and y is:", sum);
+
+console.log("End of the test file.");
